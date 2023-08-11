f20192a275f4518d7377893d6774f0500ff78319
Alejandro Martinez Belderrain <c.amartinezbelderrain@shipbob.com>
2023-08-11 15:56:28 -0300

    add games and points

---
+++ app/src/Games/Tower.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Tower = () => {
+    return (
+        <>
+            <iframe src='/games/tower' title='simon' width={'100%'} height={'100%'} frameBorder={0} />
+        </>
+    )
+}
+
+export default Tower
